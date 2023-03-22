60c25e464dfbc70ad57e8491d9872ebf130e50a6
brayanrodallega <thebrayan1a1@outlook.com>
2023-03-22 08:07:34 -0500

    :sparkles: [ADD] 404 Not Found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import '../styles/NotFound.scss'
 
 const NotFound = () => {
   return (
-    <div>NotFound, Error 404.</div>
+    <div className='container'>
+        <div id="sub-container">
+            <h1 className='title'>404</h1>
+            <h2 className='subtitle'>Not Found</h2>
+            <p>Sorry, the page you are looking for does not exist.</p>
+            <button to="/" className='primary-button'>
+                <a href='/' >Go to Home</a>
+            </button>
+        </div>
+    </div>
   )
 }
 
