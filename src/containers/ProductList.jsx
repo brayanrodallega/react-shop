a49e9910310681d3a39e66a1fd9b3df1da12e9bf
brayanrodallega <thebrayan1a1@outlook.com>
2023-03-25 22:49:09 -0500

    :sparkles: [ADD] useEffect

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProductItem from "@components/ProductItem";
 import "@styles/ProductList.scss";
+import axions from "axios";
+
+const API = "https://api.escuelajs.co/api/v1/products/"
 
 const ProductList = () => {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(async () => {
+    const response = await axions(API);
+    setProducts(response.data);
+  }, []);
+
   return (
     <section className="main-container">
       <div className="ProductList">
-        <ProductItem />
+        {products.map((product) => (
+          <ProductItem />
+        ))}
       </div>
     </section>
   );
